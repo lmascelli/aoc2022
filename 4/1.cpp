077a03a71d1e65d1d317d66e6fc53819fc6ecaeb
lmascelli <lmascelli@gmail.com>
2022-12-04 12:19:56 +0100

    day 4

---
+++ 4/1.cpp
@@ -0,0 +1,35 @@
+/*
+Leonardo Mascelli.
+Advent of Code 2022 day 4 pt 1
+
+Each line contains two pairs of number in the form x0-x1,y0-y1
+the goal is to count how many times a range is contained in the other
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  ifstream ifs("input.txt");
+  int r1[2], r2[2], count = 0;
+  string line, tmp1, tmp2;
+  stringstream tokens;
+  while(true){
+    getline(ifs, line);
+    if (ifs.eof()) break;
+    tokens = stringstream(line);
+    getline(tokens, tmp1, ',');
+    getline(tokens, tmp2);
+    r1[0] = stoi(tmp1.substr(0, tmp1.find('-')));
+    r1[1] = stoi(tmp1.substr(tmp1.find('-')+1, tmp1.length()));
+    r2[0] = stoi(tmp2.substr(0, tmp2.find('-')));
+    r2[1] = stoi(tmp2.substr(tmp2.find('-')+1, tmp2.length()));
+    count += (r1[0]>=r2[0] and r1[1]<=r2[1]) or (r2[0]>=r1[0] and r2[1]<=r1[1]);
+  }
+  cout << count << endl;
+  ifs.close();
+  return 0;
+}
