285158d5920cc1e7bbbeafe003d07f99808720ed
lmascelli <lmascelli@gmail.com>
2022-12-06 19:27:59 +0100

    day 5

--- 5/1.cpp
+++ 5/1.cpp
@@ -9,11 +9,6 @@
 #include <vector>
 using namespace std;
 
-struct crate {
-  vector<char> pile;
-  unsigned int length{0};
-};
-
 int main(int argc, char *argv[]) {
   ifstream ifs("input.txt");
   string line;
@@ -24,7 +19,7 @@ int main(int argc, char *argv[]) {
   getline(ifs, line);
   const unsigned int crates_num = line.length() / 4 + 1;
   const unsigned int line_length = crates_num * 4 - 1;
-  vector<crate> crates(crates_num);
+  vector<vector<char>> crates(crates_num);
   {
     // parsing initial state
     // temp crate for storing reversed initial state
@@ -47,15 +42,14 @@ int main(int argc, char *argv[]) {
     }
     for (unsigned int i = 0; i < crates_.size(); i++){
       for (unsigned int j = crates_[i].size(); j > 0;){
-        crates[i].pile.push_back(crates_[i][--j]);
+        crates[i].push_back(crates_[i][--j]);
       }
-      crates[i].length = crates[i].pile.size();
     }
   }
 
-  // checking the crates
+  //  checking the crates
   for (unsigned int i=0; i<crates_num; i++) {
-    for (auto el : crates[i].pile)
+    for (auto el : crates[i])
       cout << el << " ";
     cout << endl;
   }
@@ -67,11 +61,17 @@ int main(int argc, char *argv[]) {
     unsigned int from_ = line.find("f");
     unsigned int to_ = line.find("t");
     num = stoi(line.substr(5, from_ - 5));
-    cout << "num " << num;
-    from = stoi(line.substr(from_ + 4, to_ - from_ - 5));
-    cout << " from " << from;
-    to = stoi(line.substr(to_+3, line.length()-to_ - 3));
-    cout << " to " << to << endl;
+    from = stoi(line.substr(from_ + 4, to_ - from_ - 5)) -1;
+    to = stoi(line.substr(to_+3, line.length()-to_ - 3)) -1;
+    for (unsigned int i = 0; i < num; i++){
+      // move crates
+      crates[to].push_back(crates[from][crates[from].size() - 1]);
+      crates[from].pop_back();
+    }
+  }
+
+  for (auto cr: crates){
+    cout << cr[cr.size()-1];
   }
   
   ifs.close();
